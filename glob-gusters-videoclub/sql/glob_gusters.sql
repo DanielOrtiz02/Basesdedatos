dea1cc910ce9ef9b3b057c52a588bc452b28acf1
Alejandro Bonilla Quintero <abonquin1001@gmail.com>
2026-09-14 16:15:31 -0500

    feat: Enhance database creation command with character set and collation

--- glob-gusters-videoclub/sql/glob_gusters.sql
+++ glob-gusters-videoclub/sql/glob_gusters.sql
@@ -4,7 +4,7 @@
 
 
 -- Creación de la base de datos
-CREATE DATABASE IF NOT EXISTS glob_gusters;
-DELETE FROM glob_gusters;
+CREATE DATABASE IF NOT EXISTS glob_gusters DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+UsE glob_gusters;
 
-    
+
